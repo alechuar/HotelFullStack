b8108b371d9a99630aec2664888698cdd99c7d36
Alejandro <achuar@gmail.com>
2024-05-20 23:51:56 -0300

    nuevo campo tipo de habitacion y validacion individuales en formulario de reservas

--- js/index.js
+++ js/index.js
@@ -42,6 +42,7 @@ function enviarFormulario() {
     let checkout = new Date(document.getElementById("check-out").value)
     let fechaActual = new Date()
 
+    let tipoHabitacion =  document.getElementById("tipoHabitacion").value
     let cantidadAdultos = document.getElementById("adultos").value
     let cantidadMenores = document.getElementById("menores").value
     let cantidadHabitaciones = document.getElementById("habitaciones").value
@@ -55,45 +56,63 @@ function enviarFormulario() {
     }
 
     //realizamos validación de formulario(fechas y mail correctos, y resto de los campos completados)
-    if ( checkin <= fechaActual || checkout <= checkin || cantidadAdultos <= "0" || cantidadMenores < "0" || cantidadHabitaciones <= "0" || validarmail(email) === false ) {
-    document.getElementById("solicitud").textContent = 
-    `Verifique los datos ingresados`
+
+
+    if ( checkin <= fechaActual || checkout <= checkin ||isNaN(checkout) || isNaN(checkin) ) {
+       document.getElementById("solicitud").textContent = 
+       `Verifique las fechas ingresadas`
+    }else if (tipoHabitacion === "" ) {
+        document.getElementById("solicitud").textContent = 
+        `Debe seleccionar un tipo de habitación`
+    }else if (cantidadAdultos <= "0" || cantidadMenores < "0" ) {
+        document.getElementById("solicitud").textContent = 
+        `Verifique las cantidades de huespedes ingresadas`
+    } else if (cantidadHabitaciones <= "0" ) {
+        document.getElementById("solicitud").textContent = 
+        `La cantidad de habitaciones debe ser mayor a 0`
+    } else if (validarmail(email) === false ){
+        document.getElementById("solicitud").textContent = 
+       `El email no es valido`
+
     } else {
-    document.getElementById("solicitud").innerHTML = `
+        document.getElementById("solicitud").innerHTML = `
         Solicitud enviada correctamente.</br>
         El presupuesto llegará al e-mail proporcionado.`
 
-    //guardamos en sessionStorage el mail    
-    sessionStorage.setItem("mail", email)
+        //guardamos en sessionStorage el mail    
+        sessionStorage.setItem("mail", email)
 
     
-    //creamos un objeto js con la reserva (para luego crear json de datos para backend)
-    function crearObjDesdeFormulario() {
+        //creamos un objeto js con la reserva (para luego crear json de datos para backend)
+        function crearObjDesdeFormulario() {
+            
+            let objReserva = {
+                "checkin": checkin , 
+                "checkout": checkout , 
+                "tipoHabitacion": tipoHabitacion,
+                "cantidadAdultos": cantidadAdultos , 
+                "cantidadMenores": cantidadMenores , 
+                "cantidadHabitaciones": cantidadHabitaciones , 
+                "emil": email
+            } 
+            console.log(objReserva)
         
-        let objReserva = {
-            "checkin": checkin , 
-            "checkout": checkout , 
-            "cantidadAdultos": cantidadAdultos , 
-            "cantidadMenores": cantidadMenores , 
-            "cantidadHabitaciones": cantidadHabitaciones , 
-            "emil": email
-        } 
-        console.log(objReserva)
-    
-        //pasamos de objeto .js a objeto JSON
-        let reserva = JSON.stringify(objReserva)
-        console.log(reserva)
-    }
+            //pasamos de objeto .js a objeto JSON
+            let reserva = JSON.stringify(objReserva)
+            console.log(reserva)
+        }
 
-    //llamamos a la funcion crear objeto antes de limpiar valores del formulario
-    crearObjDesdeFormulario()
+        //llamamos a la funcion crear objeto antes de limpiar valores del formulario
+        crearObjDesdeFormulario()
 
-    //limpiamos los inputs del formulario
-    document.getElementById("check-in").value = ""
-    document.getElementById("check-out").value = ""
-    document.getElementById("adultos").value = ""
-    document.getElementById("menores").value = ""
-    document.getElementById("habitaciones").value = ""
-    document.getElementById("email").value = ""
+        //limpiamos los inputs del formulario
+        document.getElementById("check-in").value = ""
+        document.getElementById("check-out").value = ""
+        document.getElementById("tipoHabitacion").value = ""
+        document.getElementById("adultos").value = ""
+        document.getElementById("menores").value = ""
+        document.getElementById("habitaciones").value = ""
+        document.getElementById("email").value = ""
     }
+   
 }
